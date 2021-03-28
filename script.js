a55fe8186a01c8fc957b6a9ef190922d13235bed
Troy Freed <tfreed94@gmail.com>
2021-03-28 18:51:38 -0400

    added function that colors .row classes based on hour in the day

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function dateTime() {
   var current = curHour.format('MMM Do , YYYY ');
   curTimeDate.text(current);
 } dateTime()
+// Function that changes color of class '.row" depending on hour of day
+// Present time(red) Past time(gray) Future Time(green)
+function colorCode() {
+    time = moment().hours();
+    $(".row").each(function () {
+        var curTime = parseInt($(this).attr("id"));
+        if (curTime === time) {
+            $(this).addClass("present");
+        }
+        if (curTime > time) {
+            $(this).addClass("future")
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}colorCode()
